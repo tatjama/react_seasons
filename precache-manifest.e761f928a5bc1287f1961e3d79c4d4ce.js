337ab644bb8a98a80c69cafbd55a00df4a3265aa
tatjama <tanja120a@gmail.com>
2020-02-21 18:09:21 +0100

    Updates

---
+++ precache-manifest.e761f928a5bc1287f1961e3d79c4d4ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2c6c89d0d3d8d500e474383078132c",
+    "url": "/react_seasons/index.html"
+  },
+  {
+    "revision": "b66a1e00d74a4093ecd2",
+    "url": "/react_seasons/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d2c1f81734370a1580e0",
+    "url": "/react_seasons/static/js/2.8ebcadcb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_seasons/static/js/2.8ebcadcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66a1e00d74a4093ecd2",
+    "url": "/react_seasons/static/js/main.e51af2ae.chunk.js"
+  },
+  {
+    "revision": "e49d8391585348b6c0af",
+    "url": "/react_seasons/static/js/runtime-main.5ef33902.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_seasons/static/media/logo.5d5d9eef.svg"
+  }
+]);
